062f57c769d00ace8ad9e18c1eb05e254662ecf5
Oleg <garrbuzz@gmail.com>
2020-03-05 01:14:16 +0200

    home

--- wp-content/themes/sparrow/functions.php
+++ wp-content/themes/sparrow/functions.php
@@ -2,6 +2,7 @@
 	add_action('wp_enqueue_scripts', 'style_theme');
 	add_action('wp_footer', 'scripts_theme');
 	add_action( 'after_setup_theme', 'theme_register_nav_menu');
+	add_action( 'widgets_init', 'register_my_widgets');
 	function style_theme(){
 		wp_enqueue_style( 'style', get_stylesheet_uri());
 		wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/default.css');
@@ -18,5 +19,16 @@
 	}
 	function theme_register_nav_menu(){
 		register_nav_menu( 'topMenu', 'Навігація в хедері');
+	}
+	function register_my_widgets(){
+		register_sidebar( array(
+		'name'          => 'Right Sidebar',
+		'id'            => "right_sidebar",
+		'description'   => 'Правий сайдбар',
+		'before_widget' => '<div class="widget %2$s">',
+		'after_widget'  => "</div>\n",
+		'before_title'  => '<h5 class="widgettitle">',
+		'after_title'   => "</h5>\n"
+	) );
 	}
 ?>
